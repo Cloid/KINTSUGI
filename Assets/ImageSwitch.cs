a7d1c76b1f7750152687fb210ab3e651d96266bd
Cloid <nintendogamer@live.com>
2020-08-31 20:47:56 -0700

    Initial Commit for VN
    Initial Visual Novel code

---
+++ Assets/ImageSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ImageSwitch : MonoBehaviour
+{
+    public Image Background;
+    public Button nextImg;
+    public Button prevImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Background.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
