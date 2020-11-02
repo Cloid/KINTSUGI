756913dd5226432aaf91da9ad3e522e35d761ea4
Cloid <nintendogamer@live.com>
2020-11-02 14:32:22 -0800

    Added Visual Novel
    Intro VN has been implemented

---
+++ Assets/Scripts/ImageSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Diagnostics;
+
+public class ImageSwitch : MonoBehaviour
+{
+    public Sprite[] newImg;
+    private Image curImage;
+    public int count = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Background.enabled = false;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (count > 7)
+            {
+                SceneManager.LoadScene("Level");
+            }
+
+            curImage = this.GetComponent<Image>();
+            curImage.sprite = newImg[count];
+            count++;
+
+
+        }
+    }
+}
